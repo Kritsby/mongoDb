f0eb54e877c0a21b27c0c65bb9de557d19640af0
Kritsby <Kritsby@gmail.com>
2021-12-26 23:53:23 +1000

    Update main.go

--- main.go
+++ main.go
@@ -11,54 +11,63 @@ import (
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Testing []struct {
-	Symbol           string  `json:symbol bson:symbol`
-	Price_24h        float64 `json:price_24h bson:price_24h`
-	Volume_24h       float64 `json:volume_24h bson:volume_24h`
-	Last_trade_price float64 `json:last_trade_price bson:last_trade_price`
-}
-
 var client *mongo.Client
 
-func main() {
-	Connect()
+type Testing []struct {
+	Symbol           string  `json:"symbol" bson:"symbol"`
+	Price_24h        float64 `json:"price_24h" bson:"price_24h"`
+	Volume_24h       float64 `json:"volume_24h" bson:"volume_24h"`
+	Last_trade_price float64 `json:"last_trade_price" bson:"last_trade_price"`
 }
 
-func Connect() {
+func main() {
 	fmt.Println("Заупск приложения")
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	checkErr(err)
-	err = client.Connect(context.TODO())
-	checkErr(err)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
 	r := mux.NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", r))
+	r.HandleFunc("/test", GetRec).Methods("GET")
+	go workWithDb(client)
+	log.Fatal(http.ListenAndServe(":12345", r))
 }
 
 func GetRec(response http.ResponseWriter, request *http.Request) {
-	response.Header().Add("content-type", "application/json")
-	var recov []Testing
-	collection := client.Database("test").Collection("Testing")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	cursor, _ := collection.Find(ctx, bson.M{})
-	defer cursor.Close(ctx)
-	for cursor.Next(ctx) {
-		var testing Testing
-		cursor.Decode(&testing)
-		recov = append(recov, testing)
-	}
-	if err := cursor.Err(); err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-		return
-	}
-	json.NewEncoder(response).Encode(recov)
+	response.Header().Set("Context-Type", "application/json")
+	load := getAll()
+	json.NewEncoder(response).Encode(load)
 }
 
-func workWithDb() {
+func getAll() []primitive.D {
+	collection := client.Database("test").Collection("Testing")
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []primitive.D
+	for cur.Next(context.Background()) {
+		var result bson.D
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		results = append(results, result)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.Background())
+	return results
+}
+
+func workWithDb(client *mongo.Client) {
 
 	collection := client.Database("test").Collection("Testing")
 
